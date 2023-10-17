78523d09b06f60a63ffb54fc537637e36ca83ce2
MeheD1i <kpmh222@gmail.com>
2023-10-17 17:17:21 +0600

    email verification added

--- app/src/main/java/com/example/clinicsystem/DoctorActivity.java
+++ app/src/main/java/com/example/clinicsystem/DoctorActivity.java
@@ -2,10 +2,14 @@ package com.example.clinicsystem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,6 +21,7 @@ public class DoctorActivity extends AppCompatActivity {
     private ImageView profilePictureImageView;
     private TextView roleTextView;
     private TextView profileNameTextView; // Add this TextView for profile name
+    private TextView emailStatusTextView; // Add this TextView for email status
 
     private FirebaseAuth firebaseAuth;
 
@@ -28,11 +33,21 @@ public class DoctorActivity extends AppCompatActivity {
         profilePictureImageView = findViewById(R.id.profilePictureImageView);
         roleTextView = findViewById(R.id.roleTextView);
         profileNameTextView = findViewById(R.id.profileNameTextView); // Initialize the profileNameTextView
+        emailStatusTextView = findViewById(R.id.emailStatusTextView); // Initialize the emailStatusTextView
 
         firebaseAuth = FirebaseAuth.getInstance();
 
         // Fetch the user's role and name from the database
         fetchUserRoleAndName();
+        // Check and display email verification status
+        checkEmailVerificationStatus();
+        // Add click action to the "Email is not verified" TextView
+        emailStatusTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmailVerification();
+            }
+        });
     }
 
     private void fetchUserRoleAndName() {
@@ -57,5 +72,30 @@ public class DoctorActivity extends AppCompatActivity {
                 // Handle database error
             }
         });
+    }
+
+    private void checkEmailVerificationStatus() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            boolean isEmailVerified = user.isEmailVerified();
+
+            if (isEmailVerified) {
+                emailStatusTextView.setText("Email is verified");
+            } else {
+                emailStatusTextView.setText("Email is not verified (click here to verify)");
+            }
+        }
+    }
+
+    private void sendEmailVerification() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(this, "Verification Email Sent", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/com/example/clinicsystem/PatientActivity.java
+++ app/src/main/java/com/example/clinicsystem/PatientActivity.java
@@ -2,10 +2,14 @@ package com.example.clinicsystem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,7 +20,8 @@ public class PatientActivity extends AppCompatActivity {
 
     private ImageView profilePictureImageView;
     private TextView roleTextView;
-    private TextView profileNameTextView; // Add this TextView for profile name
+    private TextView profileNameTextView;
+    private TextView emailStatusTextView; // Add this TextView for email status
 
     private FirebaseAuth firebaseAuth;
 
@@ -27,12 +32,23 @@ public class PatientActivity extends AppCompatActivity {
 
         profilePictureImageView = findViewById(R.id.profilePictureImageView);
         roleTextView = findViewById(R.id.roleTextView);
-        profileNameTextView = findViewById(R.id.profileNameTextView); // Initialize the profileNameTextView
+        profileNameTextView = findViewById(R.id.profileNameTextView);
+        emailStatusTextView = findViewById(R.id.emailStatusTextView); // Initialize the emailStatusTextView
 
         firebaseAuth = FirebaseAuth.getInstance();
 
         // Fetch the user's role and name from the database
         fetchUserRoleAndName();
+
+        // Check and display email verification status
+        checkEmailVerificationStatus();
+        // Add click action to the "Email is not verified" TextView
+        emailStatusTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmailVerification();
+            }
+        });
     }
 
     private void fetchUserRoleAndName() {
@@ -58,5 +74,29 @@ public class PatientActivity extends AppCompatActivity {
             }
         });
     }
-}
 
+    private void checkEmailVerificationStatus() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            boolean isEmailVerified = user.isEmailVerified();
+
+            if (isEmailVerified) {
+                emailStatusTextView.setText("Email is verified");
+            } else {
+                emailStatusTextView.setText("Email is not verified (click here to verify)");
+            }
+        }
+    }
+
+    private void sendEmailVerification() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(this, "Verification Email Sent", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+}
